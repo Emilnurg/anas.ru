eba2433c0a4af60f2d67653bf34a682234934778
Raphael Kamashev <wizzzet@gmail.com>
2017-06-08 23:33:47 +0300

    убрал соцсети

--- apps/partners/admin.py
+++ apps/partners/admin.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from modeltranslation.admin import TranslationAdmin
+
+from partners import models
+from snippets.admin import BaseModelAdmin
+from snippets.modeltranslation import get_model_translation_fields
+
+
+@admin.register(models.Partner)
+class CityAdmin(BaseModelAdmin, TranslationAdmin):
+    """Города"""
+    fields = models.Partner().collect_fields()
+    list_display = ('id', 'title', 'city', 'professional_area' 'status', 'ordering', 'updated')
+    list_display_links = ('id', 'title')
+    list_filter = BaseModelAdmin.list_filter + ('city', 'professional_area')
+    ordering = ('ordering', 'title')
+    search_fields = ['=id'] + get_model_translation_fields(models.Partner)
